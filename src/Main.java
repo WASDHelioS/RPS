b85795b2be5b8af6354fca19456d17cb336bfeab
Nick <nickhiert@hotmail.com>
2022-12-18 22:41:22 +0100

    rename to Main

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 import RPS.game.Game;
 import RPS.io.IOProvider;
 
-public class Entry {
+public class Main {
 
     public static void main(String ...args) {
         Game game = new Game(new IOProvider());
